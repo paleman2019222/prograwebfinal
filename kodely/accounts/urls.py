4dfe8566562bf9c468e003843223b77cf95f6a58
Petter99 <131422787+Petter99@users.noreply.github.com>
2025-05-28 16:29:55 -0600

    migración a Firebase modular y separación de archivos JS, Creacion de funcion logout

--- kodely/accounts/urls.py
+++ kodely/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_page, login_firebase, home, create_post, ver_post
+from .views import login_page, login_firebase, home, create_post, ver_post, logout_view
 
 urlpatterns = [
     path('', login_page, name='login'), 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('crear-post/', create_post, name='crear_post'),
     path('post/<int:post_id>/', ver_post, name='ver_post'),  
+    path('logout/', logout_view, name='logout_view')
 ]
